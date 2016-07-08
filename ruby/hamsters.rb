c468e7407dfb1747c08ee2655055e53d74c3eb93
jennysoup <jennyteasoup@gmail.com>
2016-07-08 18:14:41 -0400

    Add questions about hamster

---
+++ ruby/hamsters.rb
@@ -0,0 +1,11 @@
+puts "What is the hamster's name?"
+hamster_name = gets.chomp
+puts "From 1 to 10, how loud is the hamster?"
+hamster_volume = gets.chomp
+puts "What is the color of the hamster's fur?"
+hamster_fur = gets.chomp
+puts "Is the hamster a good candidate for adoption?"
+hamster_candidate = gets.chomp
+puts "What is the hamster's estimated age?"
+hamster_age = gets.chomp
+
