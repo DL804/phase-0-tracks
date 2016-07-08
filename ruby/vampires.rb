ea37284bc147e44d39240275d83f4d1bbf5886dd
jennysoup <jennyteasoup@gmail.com>
2016-07-08 16:14:54 -0400

    Set up conditional logic for vampire detection

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,10 +1,24 @@
 puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "What year were you born?"
 birth_year = gets.chomp
+age_year_match = 2016 - birth_year.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 health = gets.chomp
+
+
+if name == "Drake Cula" || name == "Tu Fang"
+	puts "Definitely a vampire."
+elsif age == age_year_match && garlic_bread == "yes" 
+	puts "Probably not a vampire"
+elsif age != age_year_match && (garlic_bread == "no" && health == "no")
+	puts "Almost certainly a vampire"
+elsif age != age_year_match && (garlic_bread == "no" || health == "no")
+	puts "Probably a vampire"
+else puts "Results inconclusive."
+end	
+
