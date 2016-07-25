64b4df5342d7fc4041b4895aab3aee98fe6e1018
DL804 <darren804@gmail.com>
2016-07-25 12:20:02 -0700

    updated symbols

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,10 @@
+
+=begin 
+Write a program that allows an interior designer to enter details for client:
+Client's name, Age, Number of children, Decor theme, number of rooms
+=end 
+
+
 =begin 
 Write a program that allows an interior designer to enter details for client:
 Client's name, Age, Number of children, Decor theme, number of rooms
@@ -8,19 +15,19 @@ client = {}
 
 #Create new hash keys and insert value inputs 
 print "Please enter your name:"
-client["name"] = gets.chomp.capitalize
+client[:name] = gets.chomp.capitalize
 
 print "What is your age?"
-client["age"] = gets.chomp.to_i
+client[:age] = gets.chomp.to_i
 
 print "How many children do you have?"
-client["children"] = gets.chomp.to_i
+client[:children] = gets.chomp.to_i
 
 print "What type of decor theme are you interested in?"
-client["decor"] = gets.chomp
+client[:decor] = gets.chomp
 
 print "Do you have a budget(yes,no)"
-client["budget"] = gets.chomp
+client[:budget] = gets.chomp
 
 #print hash
 puts  "Here are your inputs"
@@ -35,18 +42,18 @@ user_response = gets.chomp
 #if response is one of the hash keys, get the updated response and update the hash key value. 
  	if 	user_response == "name"
 	   	puts "Please update the name"
-	   	client["name"] = gets.chomp  	
+	   	client[:name] = gets.chomp  	
  	elsif user_response == "age"
  		puts "Please update the age"
- 		client["age"] = gets.chomp
+ 		client[:age] = gets.chomp
  	elsif user_response == "children"
  		puts "Please update the number of children"
- 		client["children"] = gets.chomp
+ 		client[:children] = gets.chomp
  	elsif user_response == "decor"
  		puts "Please update the decor choice"
- 		client["decor"] == gets.chomp
+ 		client[:decor] == gets.chomp
  	elsif user_response == "budget"
- 		client["budget"] == gets.chomp
+ 		client[:budget] == gets.chomp
  	else user_response == "none"  
  		puts "All edits completed"
  	end
