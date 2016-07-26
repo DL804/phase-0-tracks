62f78f337f35a02e2db7aa1065f17b863e91f29d
DL804 <darren804@gmail.com>
2016-07-25 22:48:27 -0700

    completed release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -2,8 +2,33 @@ class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
-    toy
   end
+  
+  def speak(sound)
+  	 puts "#{sound} #{sound} #{sound}"
+  end
+  
+  def roll_over(move)
+  	puts "#{move}"
+  end 
+  
+  def dog_years(num)
+  	num = num.to_i
+  	num * 15
+  end
+  
+  def speak(word)
+  	puts "#{word}"
+  end
+  
+  
+ 
 
 end
 
+lucky = Puppy.new
+lucky.fetch("ball")
+lucky.speak("Woof")
+lucky.roll_over("*rolls over*") 
+lucky.dog_years("10")
+lucky.speak("How you doing?")
