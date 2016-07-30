08bc596d3e9fc07c918959f23a5a73d961c1ac48
DL804 <darren804@gmail.com>
2016-07-29 19:05:12 -0700

    final edits

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,35 @@
+class TodoList
+  
+attr_reader :list
+  
+  def initialize(list)
+   @list = list
+  end
+
+  def get_items
+    list
+  end
+
+  def add_item(item)
+  	list << item
+  end 
+
+  def delete_item(item)
+  	list.delete(item)
+  end
+  
+  def get_item(index)
+  	list[index]
+  end
+
+end
+
+list = ["do the dishes","mow the lawn"]
+test = TodoList.new(list)
+p test.list
+test.add_item("mop")
+p test.list
+test.delete_item("do the dishes")
+p test.list
+
+
