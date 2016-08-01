e8a1bb09c96f245ea1fbcb551bc6ed3135e688e6
DL804 <darren804@gmail.com>
2016-07-31 23:31:54 -0700

    solo spec

--- ruby/solo/solo_spec.rb
+++ ruby/solo/solo_spec.rb
@@ -2,12 +2,17 @@ require_relative 'solo'
 
 
 describe Word do
-  let(:player) { Word.new (["wo"])}
+  let(:player) { Word.new ("word")}
 
 
-  it "input letter and output spaces and letter" do
-  	player.check_user2input("o")
-    expect(player.printstatus).to eq ["_w"]
+ # it "input letter and output spaces and letter" do
+ # 	player.check_user2input("o")
+ #   expect(player.printstatus).to eq ["_w"]
+ # end
+  it "stores the user1input" do
+    expect(player.player1input).to eq "word"
   end
+
+
 
 end
